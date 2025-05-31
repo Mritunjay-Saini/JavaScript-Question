60019df48b6dddc9db11b2befd19922cda1a96e7
Mritunjay Saini <140881890+Mritunjay-Saini@users.noreply.github.com>
2025-05-31 08:01:44 +0530

    Delete Q15.js

--- Q15.js
+++
@@ -1,5 +0,0 @@
-// Write a function that accepts an array and returns a string of all elements joined with ' |
-// '.
-const arr = ["ram","sham","yash","rahul"];
-const result = arr.join("|")
-console.log(result)
