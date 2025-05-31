508d057e7be5708ad1a1574e1fe015b075031f6f
Mritunjay Saini <140881890+Mritunjay-Saini@users.noreply.github.com>
2025-05-31 08:02:30 +0530

    Delete Q16.js

--- Q16.js
+++
@@ -1,8 +0,0 @@
-// Create a function that returns the last item of an array using only .length.
-
-function getLastItem(arr) {
-  return arr[arr.length - 1];
-}
-
-//Example:
-console.log(getLastItem([10, 20, 30]));
